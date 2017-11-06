304b451586a57244a041788a5e0c764e9de9dd84
lailanelkoussy <lailanelkoussy@aucegypt.edu>
2017-11-06 18:13:58 +0200

    Added opening function for file and declared file in main and opened it

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+void opening (ifstream &file, const string& filename);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    ifstream file;
+    string filename = "wordlist.txt";
+    opening (file, filename);
+
+
     return 0;
+}
+
+
+void opening (ifstream &file, const string& filename)
+{ file.open(filename);
+    if (file.is_open())
+        return;
+    else {
+        cout << "Could not open " << filename;
+        return;
+    }
+
 }
