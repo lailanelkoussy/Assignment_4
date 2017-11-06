156098753a9e52ba6c41d3242c1ef81f1068f9ec
lailanelkoussy <lailanelkoussy@aucegypt.edu>
2017-11-06 19:01:14 +0200

    Edited hashTable class

--- hashTable.cpp
+++ hashTable.cpp
@@ -3,3 +3,49 @@
 //
 
 #include "hashTable.h"
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+template <class keyType, class dataType>
+hashTable<keyType, dataType>::hashTable(int nelements)
+{
+    MaxSize = nelements;
+    T = new slot[MaxSize];
+    h = -1;
+    csize = 0;
+}
+
+template <class keyType, class dataType>
+void hashTable<keyType, dataType>::insert(const keyType &k, const dataType &d)
+{
+
+        h = hash (k);
+        csize++;
+
+        if (T[h].key == Empty) {
+            T[h].key = k;
+            T[h].data = d;
+            return;
+        }
+        else
+        {
+            slot * temp;
+
+        }
+
+
+
+    }
+
+template <class keyType, class dataType>
+int hashTable<keyType, dataType>::hash(const string &k) const
+{ int temp = 1;
+    for (int i = 0; i<k.length(); i++)
+        temp = int(k[i]) * pow(26, i);
+        temp %= MaxSize;
+
+
+    return temp;
+}
--- hashTable.h
+++ hashTable.h
@@ -5,8 +5,40 @@
 #ifndef ASSIGNMENT_4_HASHTABLE_H
 #define ASSIGNMENT_4_HASHTABLE_H
 
+template <class keyType, class dataType>
 
 class hashTable {
+public:
+
+    // Member Functions
+    hashTable(int nelements = 149);		// Constructor
+    ~hashTable();						// Destructor
+
+    // Functions Prototype Definitions
+
+    void makeTableEmpty(const keyType &);
+    bool tableIsEmpty() const;
+    bool tableIsFull() const;
+    void insert(const keyType &, const dataType &);
+    bool search(const keyType &);
+
+private:
+
+    // Slot Class
+    class slot
+    {
+    public:
+        keyType key; 		// key
+        dataType data;		// Data
+        slot * next;
+    }; // end of class slot declaration
+
+    slot *T;							// Pointer to Storage Array
+    int h;								// Index to a slot
+    int MaxSize, csize;					// Maximum and Current Sizes
+    keyType Empty;						// empty symbol
+
+    int hash(const keyType &) const;
 
 };
 
